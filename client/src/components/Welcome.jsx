60073b6a4ca0d900973538072144f75f37a4ba8a
agaskrobot <agnieszka.skrobot1@gmail.com>
2022-07-18 20:05:07 +0200

    frontend part prepared

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -31,7 +31,7 @@ export const Welcome = () => {
 			<div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
 				<div className="flex flex-1 justify-start flex-col mf:mr-10">
 					<h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-						Send Crypto <br /> across the world
+						Send Ethereum <br /> across the world
 					</h1>
 					<p className="text-letf mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
 						Explore the crypto world. Buy and sell cryptocurrencies easily on AgaCrypt.
@@ -39,7 +39,7 @@ export const Welcome = () => {
 					<button
 						type="button"
 						onClick={connectWallet}
-						className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+						className="flex flex-row justify-center items-center my-5 bg-blue-400 p-3 rounded-full cursor-pointer hover:bg-blue-500"
 					>
 						<p className="text-white text-base font-semibold">Connect Wallet</p>
 					</button>
